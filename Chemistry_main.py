6234ad9200257712d88574d2f56762f61b1d35d9
Klojjj <115587305+Klojjj@users.noreply.github.com>
2022-10-13 18:42:29 +0700

    Update 13.10.2022
    Friendlier interface, now can solve tasks with multiple amount of substance

--- Chemistry_main.py
+++ Chemistry_main.py
@@ -7,15 +7,16 @@ t_2 = int(input())
 print('Please, enter number of reaction components:')
 n = int(input())
 print('PLEASE, ENTER REACTANT CONSTANTS WITH OPPOSITE SIGN!!!')
-a_r, b_r, c_r = chem.calculate_capacity(n)
-print('Choose one option:', '1 - calculate entropy', '2 - calculate enthalpy', "3 - calculate Gibb's energy", '4 - all')
+a_r, b_r, c_r, k = chem.calculate_capacity(n)
+print('Choose one option:')
+print('1 - calculate entropy', '2 - calculate enthalpy', "3 - calculate Gibb's energy", '4 - all', sep='\n')
 flag = int(input())
 if flag in (1, 3, 4):
-    entropy = chem.calculate_entropy(a_r, b_r, c_r, t_1, t_2, n)
+    entropy = chem.calculate_entropy(a_r, b_r, c_r, t_1, t_2, n, k)
     if flag in (1, 4):
         print("Entropy = ", entropy * 10 ** 4 // 10 / 10 ** 3, "J")
 if flag in (2, 3, 4):
-    enthalpy = chem.calculate_enthalpy(a_r, b_r, c_r, t_1, t_2, n)
+    enthalpy = chem.calculate_enthalpy(a_r, b_r, c_r, t_1, t_2, n, k)
     if flag in (2, 4):
         print("Enthalpy = ", enthalpy * 10 ** 4 // 10 / 10 ** 3, "kJ")
 if flag in (3, 4):
--- chem.py
+++ chem.py
@@ -1,21 +1,21 @@
-def calculate_entropy(a_r, b_r, c_r, t_1, t_2, n):
+def calculate_entropy(a_r, b_r, c_r, t_1, t_2, n, k):
     from math import log
     entropy = 0
     for i in range(n):
-        print('Please, enter initial entropy #', i, ' :', sep='')
+        print('Please, enter initial entropy #', i + 1, ' :', sep='')
         entropy_initial = float(input().replace(',', '.'))
-        entropy += entropy_initial
+        entropy += entropy_initial * k[i]
     entropy_delta = a_r * log(t_2 / t_1) + b_r * (t_2 - t_1) + c_r / 2 * (t_2 ** -2 - t_1 ** -2)
     entropy += entropy_delta
     return entropy
 
 
-def calculate_enthalpy(a_r, b_r, c_r, t_1, t_2, n):
+def calculate_enthalpy(a_r, b_r, c_r, t_1, t_2, n, k):
     enthalpy = 0
     for i in range(n):
-        print('Please, enter initial enthalpy #', i, ' :', sep='')
+        print('Please, enter initial enthalpy #', i + 1, ' :', sep='')
         enthalpy_initial = float(input().replace(',', '.'))
-        enthalpy += enthalpy_initial
+        enthalpy += enthalpy_initial * k[i]
     enthalpy_delta = (a_r * (t_2 - t_1) + b_r / 2 * (t_2 ** 2 - t_1 ** 2) + c_r * (1 / t_2 - 1 / t_1)) / 10 ** 3
     enthalpy += enthalpy_delta
     return enthalpy
@@ -31,14 +31,17 @@ def calculate_gibbs_energy(entropy, enthalpy):
 
 def calculate_capacity(n):
     a_r, b_r, c_r = 0, 0, 0
+    k = []
     for i in range(n):
-        print('Please, enter "a #', i, '" :', sep='')
+        print('Please, enter "a #', i + 1, '" :', sep='')
         a = float(input().replace(',', '.'))
-        print('Please, enter "b #', i, '" :', sep='')
+        print('Please, enter "b #', i + 1, '" :', sep='')
         b = float(input().replace(',', '.')) / 10 ** 3
-        print('Please, enter "c #', i, '" :', sep='')
+        print('Please, enter "c #', i + 1, '" :', sep='')
         c = float(input().replace(',', '.')) * 10 ** 5
-        a_r += a
-        b_r += b
-        c_r += c
-    return a_r, b_r, c_r
+        print('Please, enter amount of substance #', i + 1, ':', sep='')
+        k.append(float(input().replace(',', '.')))
+        a_r += a * k[i]
+        b_r += b * k[i]
+        c_r += c * k[i]
+    return a_r, b_r, c_r, k
